5df287e90f158d7f455fab5df4da1cd6e9263be0
Dexter Chan <dexterchann@gmail.com>
2020-04-27 22:15:40 +0000

    add comments

--- lib/employee.rb
+++ lib/employee.rb
@@ -5,14 +5,14 @@ class Employee < ActiveRecord::Base
   validates :hourly_rate, numericality: { greater_than_or_equal_to: 40, less_than_or_equal_to: 200}
 
   before_create :set_random_password
-  # after_create :set_random_password
-  # before_save :set_random_password
+  # after_create :set_random_password # fires after the database has been updated
+  # before_save :set_random_password # fires on create AND update
 
   private
     def set_random_password
       random_password = rand(36**8).to_s(36)
       self.password = random_password
-      # self.update(password: random_password)
+      # self.update(password: random_password) # use with after_create
     end
 
 
